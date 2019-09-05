979213ba1f8650a6d7f70f30642cdc2c8a4fba5d
mattias <mattias.decharleroy@gmail.com>
2019-09-05 18:52:43 +0200

    Update todo and proposed db schema changes + revamped bootstrap script (dc.py)

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Een bierkroeg service waarbij bestellingen gemaakt en gevolgd kunnen worden.
 ### Quick Start
 
 1) In terminal: git clone https://github.com/MattiasDC/BierkroegService.git
-2) Add DB_URL and FLASK_SECRET_KEY environment variables (either by providing it by .env file or alternate docker-compose files)
-3) Make sure the database is running and accessible via the specified url
+2) Make sure the database is running and accessible
+3) Make sure docker secrets are available (by providing an alternate docker-compose)
 4) In terminal: python dc.py
+5) Follow any setup instructions
+6) Open web browser at localhost:5004
--- dc.py
+++ dc.py
@@ -1,9 +1,14 @@
 import click
+from functools import reduce
+import os
 import subprocess
 
+"""
+Convenience function to execute the given command in an interactive bash
+"""
 def runCommand(command):    
     p = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    while(True):
+    while True:
         # returns None while subprocess is running
         retcode = p.poll() 
         line = p.stdout.readline()
@@ -11,35 +16,47 @@ def runCommand(command):
         if retcode is not None:
             break
 
-@click.command()
-@click.option("--build", is_flag=True, help="Build before running.")
-@click.option("--prod", is_flag=True, help="Run/Build in production.")
-@click.option("--user", help="The user to build/run for.")
-def dc(build, prod, user):
-	dcString = "docker-compose"
-	extension = ".yml"
-	
-	command = ""
-	if build:
-		command += "sudo "
-	command += dcString + " -f " + dcString + extension
+def getDockerComposeFile(directory, specifications):
+	specifications.insert(0, "docker-compose")
+	specifications.append("yml")
+	specificationString = reduce(lambda tot, spec: tot + "." + spec, specifications)
+	return directory + "/" + specificationString
+
+def getMainDockerComposeFile():
+	return getDockerComposeFile(".", [])
+
+def addDockerComposeFile(command, dockerComposeFile):
+	command += " -f " + dockerComposeFile
+	return command
+
+def buildCommand(build, prod, user):
+	command = "docker-compose"
+	command += addDockerComposeFile("", getMainDockerComposeFile())
 
 	if prod:
-		flavor = "prod"
+		raise(NotImplementedError("Production support is not yet implemented"))
 	else:
 		flavor = "dev"
-	command += " -f " + flavor + "/" + dcString + "." + flavor + extension
+	command = addDockerComposeFile(command, getDockerComposeFile(flavor, [flavor]))
 
-	if user is not None: 
-		command += " -f " + flavor + "/" + dcString + "." + flavor + "." + user + extension
+	if user is not None:
+		command = addDockerComposeFile(command, getDockerComposeFile(flavor, [flavor, user]))
 
 	command += " up"
 
 	if build:
 		command += " --build"
-	
+	return command
+
+@click.command()
+@click.option("--build", is_flag=True, help="Build before running.")
+@click.option("--prod", is_flag=True, help="Run/Build in production.")
+@click.option("--user", help="The user to build/run for.")
+def dc(build, prod, user):
+	command = buildCommand(build, prod, user)
+	print(command)
 	for line in runCommand(command):
 		click.echo(line)
 
 if __name__ == '__main__':
-    dc()
+	dc()
